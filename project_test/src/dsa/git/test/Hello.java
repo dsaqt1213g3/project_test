77eca18d6a21b3329da82c90557c583e31b46750
Albert Raventos <raventos.albert@gmail.com>
2012-10-08 12:07:53 +0200

    Commit prueba Albert

---
+++ project_test/src/dsa/git/test/Hello.java
@@ -0,0 +1,17 @@
+package dsa.git.test;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
