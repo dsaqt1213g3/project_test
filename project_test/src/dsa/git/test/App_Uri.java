dbdee2c4d027bf5f45eea83fe21c2cd3cdbd6100
FRG <FRG@FRG-PC>
2012-10-16 17:34:20 +0200

    modificaciones1

--- project_test/src/dsa/git/test/App_Uri.java
+++ project_test/src/dsa/git/test/App_Uri.java
@@ -2,8 +2,10 @@ package dsa.git.test;
 
 public class App_Uri {
 
+	int id;
 	public App_Uri() {
 		// TODO Auto-generated constructor stub
+		id = 0;
 	}
 
 	/**
@@ -11,7 +13,8 @@ public class App_Uri {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		App_Uri uri = new App_Uri();
+		uri.id = 20;
 	}
 
 }
